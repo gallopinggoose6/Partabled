332df5999c4f372fa87b82286abce6cbaad96333
Probably a Penguin <nicolas.p.ammann@gmail.com>
2021-08-11 16:32:49 -0600

    added notes_to_self.txt

--- src/fs/fat32.rs
+++ src/fs/fat32.rs
@@ -1,1 +1,5 @@
 // Includes structs and APIs for handling FAT32 partitions
+// note, this could simply be a wrapper around the UEFI SimpleFileSystem protocol
+
+// notes on how it works
+// https://wiki.osdev.org/FAT
