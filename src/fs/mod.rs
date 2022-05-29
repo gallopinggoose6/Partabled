8c82a4e5366ab1a7bb8db83f6f6920cdfbce315b
BingoChado <nicolas.p.ammann@gmail.com>
2022-05-28 23:00:58 -0600

    Reworked filesystem interface

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,3 +1,5 @@
+use alloc::{string::String, vec::Vec};
+
 /* 
 NOTE THIS IS POTENTIALLY GONNA BE GOING UP INTO THE AIR
 AND EXPLODING WITH VARYING DEGREES OF "WE DONT NEED THIS"
@@ -8,14 +10,17 @@ pub mod ext4;
 pub mod fat32;
 
 
+/// define raw file structure
+pub struct RawFile{
+    path: String,
+    size: usize,
+    data: Vec<u8>
+}
+
 /// define our Filesystem traits
 pub trait Filesystem {
     /// creates a new instance of the filesystem
     fn new(media_id: u32, lba_start: u64, lba_end: u64) -> Self;
 
-    /// defines the root of the filesystem
-    fn root(&self);
-
-    /// defines an iterator that gets all of the files in a directory
-    fn file_iter(&self);
+    fn get_file(&self, path: String) -> RawFile;
 }
